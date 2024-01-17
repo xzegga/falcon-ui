462c574d79a0709182bc9b1073594b99b7f14be9
Jose Ramirez <jose.ramirez3@concentrix.com>
2024-01-17 14:32:21 -0600

    upload a video to supbase

--- src/components/composite/screenEmotions.tsx
+++ src/components/composite/screenEmotions.tsx
@@ -1,21 +1,49 @@
 import {useRef,useEffect, useState} from 'react'
 import * as faceapi from 'face-api.js'
-
-export default function ScreenEmotions(){
-    const videoRef = useRef<HTMLVideoElement>(null);
+import { useUploadFile } from "@/lib/store";
+export default function ScreenEmotions({ id }: { id: string }) {
+  const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [emotions, setEmotions] = useState<{ emotion: string; date: Date }[]>([]);
+  const [emotions, setEmotions] = useState<{ emotion: string; date: Date }[]>(
+    []
+  );
+  const { loading, result, error, uploadFile } = useUploadFile() as any;
+  const [recording, setRecording] = useState<boolean>(false);
 
   useEffect(() => {
     startVideo();
     videoRef.current && loadModels();
   }, []);
 
+  let mediaRecorder = useRef<MediaRecorder | undefined>();
+  let chunks = [] as BlobPart[];
+
   const startVideo = () => {
-    navigator.mediaDevices.getUserMedia({ video: true })
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
       .then((currentStream) => {
         if (videoRef.current) {
           videoRef.current.srcObject = currentStream;
+          //get current stream to somehow record
+
+          mediaRecorder.current = new MediaRecorder(currentStream);
+
+          mediaRecorder.current.ondataavailable = (event) => {
+            if (event.data.size > 0) {
+              chunks.push(event.data);
+            }
+          };
+
+          mediaRecorder.current.onstop = () => {
+            const videoBlob = new Blob(chunks, { type: "video/webm" });
+            uploadFile({ id, file: videoBlob });
+            // const downloadLink = document.createElement("a");
+            // downloadLink.href = URL.createObjectURL(videoBlob);
+            // downloadLink.download = "captured-video.webm";
+            // downloadLink.click();
+
+            chunks = [];
+          };
         }
       })
       .catch((err) => {
@@ -26,25 +54,32 @@ export default function ScreenEmotions(){
   const loadModels = async () => {
     try {
       await Promise.all([
-        faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-        faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-        faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-        faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+        faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+        faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+        faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+        faceapi.nets.faceExpressionNet.loadFromUri("/models"),
       ]);
 
       faceMyDetect();
     } catch (error) {
-      console.error('Error loading models:', error);
+      console.error("Error loading models:", error);
     }
   };
 
   const faceMyDetect = () => {
     setInterval(async () => {
       if (videoRef.current && canvasRef.current) {
-        const detections = await faceapi.detectAllFaces(videoRef.current,
-          new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        const detections = await faceapi
+          .detectAllFaces(
+            videoRef.current,
+            new faceapi.TinyFaceDetectorOptions()
+          )
+          .withFaceLandmarks()
+          .withFaceExpressions();
 
-        canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(videoRef.current);
+        canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(
+          videoRef.current
+        );
         faceapi.matchDimensions(canvasRef.current, {
           width: 900,
           height: 650,
@@ -53,9 +88,12 @@ export default function ScreenEmotions(){
         const [myemotion] = detections;
         if (myemotion) {
           const { expressions } = myemotion;
-          const emotionDetected = Object.keys(expressions).reduce((acc, value) => {
-            return (expressions[acc] > expressions[value] ? acc : value);
-          }, '');
+          const emotionDetected = Object.keys(expressions).reduce(
+            (acc, value) => {
+              return expressions[acc] > expressions[value] ? acc : value;
+            },
+            ""
+          );
 
           const emotionObj = { emotion: emotionDetected, date: new Date() };
           setEmotions((current) => [...current, emotionObj]);
@@ -64,17 +102,32 @@ export default function ScreenEmotions(){
     }, 1000);
   };
 
-  useEffect(() => {
-    console.log(emotions);
-  }, [emotions]);
+  const handleStopRecording = () => {
+    if (!recording) {
+      mediaRecorder.current?.start();
+      setRecording(true);
+    } else {
+      mediaRecorder.current?.stop();
+      setRecording(false);
+    }
+  };
 
-    return (
-        <div className="myapp">
-        <h1>Face Detection</h1>
-        <div className="appvide">
-          <video crossOrigin="anonymous" ref={videoRef} autoPlay />
-        </div>
-        <canvas ref={canvasRef} width="400" height="300" className="appcanvas" />
+  return (
+    <div className="myapp">
+      <button
+        className="border-full p-1 border-red-900 bg-red-800"
+        onClick={handleStopRecording}
+      >
+        {!recording ? "record" : "recording..."}
+      </button>
+      <p>{loading ? "uploading..." : ""}</p>
+      <p>{error ? error.message || "hey, error" : ""}</p>
+
+      <h1>Face Detection</h1>
+      <div className="appvide">
+        <video crossOrigin="anonymous" ref={videoRef} autoPlay muted />
       </div>
-        )
+      <canvas ref={canvasRef} width="400" height="300" className="appcanvas" />
+    </div>
+  );
 }
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,8 +1,33 @@
 "use client"
 
 import { create } from 'zustand'
+import axios from "axios";
 
 export const useGlobalStore = create((set) => ({
   someValue: null,
-  updateSomeValue: () => set((state:any) => ({ someValue: state.someValue })),
-}))
+  updateSomeValue: () => set((state: any) => ({ someValue: state.someValue })),
+}));
+
+export const useUploadFile = create((set) => ({
+  loading: false,
+  error: undefined,
+  result: undefined,
+
+  uploadFile: async (payload: { id: string; file: File }) => {
+    set({ loading: true });
+    const { id, file } = payload;
+    const formData = new FormData();
+
+    formData.append("id", id);
+    formData.append("file", file);
+
+    try {
+      const response = await axios.post("/api/UploadFile", formData);
+      set({ result: response.data });
+      set({ loading: false });
+    } catch (e) {
+      set({ error: e });
+      set({ loading: false });
+    }
+  },
+}));
