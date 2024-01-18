21ffbff28eaf9511d33d5f56e84bfc20e0ba4554
Alejandro <alejandro.gonzalez2@concentrix.com>
2024-01-18 11:37:12 -0600

    navbar link to root

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,15 +1,18 @@
+"use client";
 import Image from "next/image";
 import React from "react";
+import { useRouter } from "next/navigation";
 
-const navbar = () => {
+export default function Navba(){
+  const router = useRouter();
   return (
     <nav
-      className="preflight relative flex h-12 w-full items-center gap-2 bg-gray-500 px-2 text-white"
+      className="preflight relative flex h-12 w-full items-center gap-2 bg-gray-500 px-2 text-white cursor-pointer"
       data-te-navbar-ref
+      onClick={() => router.push("/")}
     >
         <Image alt="ConcentrixCX" width={145} height={45} src="/assets/cnx-logo.png"/> | <h1 className="text-xl text-[#E7E8EA]">Falcon<span className="font-bold text-white">AI</span></h1>
     </nav>
   );
 };
 
-export default navbar;
