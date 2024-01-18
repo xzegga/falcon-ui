cfe34e16175467d747509406822c4fbd779287ed
Alejandro <alejandro.gonzalez2@concentrix.com>
2024-01-18 11:16:46 -0600

    layout width adjustment

--- src/app/client/talk-carly/page.tsx
+++ src/app/client/talk-carly/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Disclaimer from "@/components/composite/disclaimer";
 import Summary from "@/components/composite/summary";
+import Verbatim from "@/components/composite/verbatim";
 import Back from "@/components/ui/backButton";
 import React, { useEffect, useState } from "react";
 
@@ -48,8 +49,9 @@ export default function TalkCarly() {
         <Back />
         <Disclaimer message="This is a disclaimer - Talk Carly" />
       </div>
-      <div className="flex w-full justify-center">
+      <div className="w-3/4 mx-auto">
         <Summary text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
+        <Verbatim />
       </div>
       <div>
         <button onClick={isListening ? stopListening : startListening}>
