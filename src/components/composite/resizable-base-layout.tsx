52a91b12ce30549225f260ca9de4291e76b5f263
Alejandro <alejandro.gonzalez2@concentrix.com>
2024-01-18 16:53:29 -0600

    WIP: revamp admin

--- src/components/composite/resizable-base-layout.tsx
+++ src/components/composite/resizable-base-layout.tsx
@@ -23,9 +23,9 @@ export function ResizableBaseLayout({
   const defaultLayout = [30, 70];
 
   const { loadingdb, resultdb, errordb, get } = useSurvey() as any;
-  useEffect(()=>{
-      get();
-  },[]);
+  useEffect(() => {
+    get();
+  }, []);
 
   const today = new Date();
   const recordings = [
@@ -50,90 +50,51 @@ export function ResizableBaseLayout({
     {
       id: "484f4749-e366-474f-be17-fab60d05beb0",
       title: "By WebCam",
-      transcription:
-        "capturing from WebCam.",
+      transcription: "capturing from WebCam.",
       type: "video",
       source: "webcam",
       created_at: `${today.getHours()}:${today.getMinutes()}`,
     },
   ];
+
   return (
-    <div>
-      {
-        resultdb ? (<ResizablePanelGroup
-          direction="horizontal"
-          onLayout={(sizes: number[]) => {
-            document.cookie = `react-resizable-panels:layout=${JSON.stringify(
-              sizes
-            )}`;
-          }}
-          className="h-full max-h-[800px] items-stretch"
-        >
-          <ResizablePanel defaultSize={defaultLayout[0]} minSize={30}>
-            <Tabs defaultValue="all">
-              <div className="flex items-center px-4 py-2">
-                <h1 className="text-xl font-bold">Recordings</h1>
-                <TabsList className="ml-auto">
-                  <TabsTrigger
-                    value="all"
-                    className="text-zinc-600 dark:text-zinc-200"
-                  >
-                    All
-                  </TabsTrigger>
-                  <TabsTrigger
-                    value="live"
-                    className="text-zinc-600 dark:text-zinc-200"
-                  >
-                    Live
-                  </TabsTrigger>
-                  <TabsTrigger
-                    value="file"
-                    className="text-zinc-600 dark:text-zinc-200"
-                  >
-                    File
-                  </TabsTrigger>
-                </TabsList>
+    <>
+      {resultdb && (
+        <div className="flex flex-row">
+          <div className="w-1/4">
+            <div className="flex justify-between items-center px-4 py-2 border-b border-gray-500">
+              <h2 className="text-xl text-secondary-600">Recordings</h2>
+              <div className="flex gap-1">
+                <Button variant={"link"}>All</Button>
+                <Button variant={"link"}>Live</Button>
+                <Button variant={"link"}>File</Button>
               </div>
-              <Separator />
-              <div className="grid grid-cols-2 gap-4 justify-center pt-4 pr-4 pl-4">
-                <Button className="">
-                  <Radio className="mr-2 h-4 w-4" />
-                  New Live
-                </Button>
-                <Button className="">
-                  <File className="mr-2 h-4 w-4" />
-                  New File
-                </Button>
-              </div>
-              <div className="bg-background/95 p-4 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-                <form>
-                  <div className="relative">
-                    <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
-                    <Input placeholder="Search" className="pl-8" />
-                  </div>
-                </form>
-              </div>
-              <TabsContent value="all" className="m-0">
-                <RecordingList items={resultdb} />
-              </TabsContent>
-              <TabsContent value="live" className="m-0">
-                <RecordingList
-                  items={resultdb.filter((item: any) => item.type === "live")}
-                />
-              </TabsContent>
-              <TabsContent value="file" className="m-0">
-                <RecordingList
-                  items={resultdb.filter((item: any) => item.type === "file")}
-                />
-              </TabsContent>
-            </Tabs>
-          </ResizablePanel>
-          <ResizableHandle withHandle />
-          <ResizablePanel defaultSize={defaultLayout[1]}>
-            {children}
-          </ResizablePanel>
-        </ResizablePanelGroup>) : null
-      }
-      </div>
+            </div>
+            <div className="px-4 py-2 flex gap-2">
+              <Button className="rounded w-1/2">
+                <Radio className="mr-2 h-4 w-4" />
+                New Live
+              </Button>
+              <Button className="rounded w-1/2">
+                <File className="mr-2 h-4 w-4" />
+                New File
+              </Button>
+            </div>
+            <div className="px-4 py-2">
+              <form className="w-full">
+                <div className="relative">
+                  <Search className="absolute left-2 top-2.5 h-4 w-4 text-muted-foreground" />
+                  <Input placeholder="Search" className="pl-8" />
+                </div>
+              </form>
+            </div>
+            <div className="mt-8 py-4 bg-slate-300 overflow-y-scroll h-[calc(100vh-300px)]">
+              <RecordingList items={resultdb} />
+            </div>
+          </div>
+          <div className="w-3/4">{children}</div>
+        </div>
+      )}
+    </>
   );
 }
