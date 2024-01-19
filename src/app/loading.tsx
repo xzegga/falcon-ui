5c1068e097a11e6106362d925b9e5cf9d5762703
Raul Escamilla <raul.escamilla@asesoriait.com>
2024-01-19 11:58:59 -0600

    unnused files removed

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,8 +3,8 @@ import { Spinner } from "@nextui-org/react";
 export default function Loading() {
   // You can add any UI inside Loading, including a Skeleton.
   return (
-    <p className="flex min-h-screen items-center justify-center text-gray-800 p-24">
+    <div className="flex min-h-screen items-center justify-center text-gray-800 p-24">
       <Spinner color="secondary" size="lg" />
-    </p>
+    </div>
   );
 }
