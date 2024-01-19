4287c89900dc985d575783284a32dc080a04fee2
fidel.mejia.valladares <fidel.mejia.valladares@gmail.com>
2024-01-18 23:10:05 -0600

    fixing topics

--- src/lib/dataSetFormatter.ts
+++ src/lib/dataSetFormatter.ts
@@ -1,37 +1,45 @@
+
 export function dataSetFormatter(dataset: Array<any>) {
-  if (dataset.length > 1) {
     const [first] = dataset;
-    if (first) {
-      const headers = Object.keys(first);
-      const values = dataset.map((item: any) => [item.emotion, item.value]);
-      return [headers, ...values];
+    const headers = Object.keys(first)
+
+    const values = dataset.map((Items ) => {
+        const value = []
+        for (const key in Items) {
+            value.push(Items[key])
+        }
+        return value;
     }
-  }
+    );
+
+    const dataSetEchart = [headers, ...values]
+
+    return dataSetEchart;
 }
 
 export function dataSetTotals(dataset: Array<any>) {
-  dataset.shift();
-  const summary: any = {};
-  for (const value of dataset) {
-    const [emotion] = value;
-    summary[emotion] ? summary[emotion]++ : (summary[emotion] = 1);
-  }
-  const pieDataSet = [];
-  for (const val of Object.keys(summary)) {
-    pieDataSet.push({ value: summary[val], name: val });
-  }
+    dataset.shift()
+    const summary: any = {}
+    for (const value of dataset){
+        const [emotion]=value
+        summary[emotion] ? summary[emotion]++ : summary[emotion] = 1
+    }
+    const pieDataSet = []
+    for(const val of Object.keys(summary)){
+        pieDataSet.push({value:summary[val],name: val})
+    }
 
-  return pieDataSet;
+    return pieDataSet;
 }
 
-export function topicsFormat(topics: any) {
-  const topicValues: Array<any> = [];
-  topics.forEach((item: any) => {
-    topicValues.push(item);
-  });
-  const topicsDataSet: any = dataSetFormatter(topicValues);
-  return {
-    topicDataSet: topicsDataSet,
-    topicValues: topicValues,
-  };
-}
+export function topicsFormat(topics: any){
+    const topicValues: Array<any> = []
+    topics.forEach((item: any)=>{
+      topicValues.push(item)
+    })
+    const topicsDataSet: any = dataSetFormatter(topicValues)
+    return {
+      topicDataSet: topicsDataSet,
+      topicValues: topicValues
+    }
+  }
