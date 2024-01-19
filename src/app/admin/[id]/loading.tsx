f3feecc5aa56579b5f57b623d30f728e061a2819
Alejandro <alejandro.gonzalez2@concentrix.com>
2024-01-19 11:13:04 -0600

    loading spinner

--- src/app/admin/[id]/loading.tsx
+++ src/app/admin/[id]/loading.tsx
@@ -1,8 +1,10 @@
+import { Spinner } from "@nextui-org/react";
+
 export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
-    return (
-        <>
-        <p className="text-sm leading-tight text-muted-foreground"> Loading... </p>
-        </>
-    )
-  }
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <p className="flex min-h-screen items-center justify-center text-gray-800 p-24">
+      <Spinner color="secondary" size="lg" />
+    </p>
+  );
+}
--- src/components/composite/resizable-base-layout.tsx
+++ src/components/composite/resizable-base-layout.tsx
@@ -1,27 +1,13 @@
 "use client";
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable";
-import { Separator } from "@/components/ui/separator";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Input } from "@/components/ui/input";
-import { Radio, Search } from "lucide-react";
 import { RecordingList } from "@/components/composite/recording-list";
-import { Button } from "@/components/ui/button";
-import { File } from "lucide-react";
-import Navbar from "../ui/navbar";
 import { useEffect } from "react";
 import { useSurvey } from "@/lib/db";
-import { dataSetTotals } from "@/lib/dataSetFormatter";
 
 export function ResizableBaseLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const defaultLayout = [30, 70];
 
   const { loadingdb, resultdb, errordb, get } = useSurvey() as any;
   useEffect(() => {
@@ -29,34 +15,6 @@ export function ResizableBaseLayout({
   }, []);
 
   const today = new Date();
-  const recordings = [
-    {
-      id: "c815cb17-2634-409e-9c3f-56ac1de5f222",
-      title: "Did you got the assistance you needed?",
-      transcription:
-        "Yes, I got the assistance I needed. I am very happy with the service provided by the agent. I would like to give a 5 star rating to the agent. Thank you.",
-      type: "video",
-      source: "file",
-      created_at: `${today.getHours()}:${today.getMinutes()}`,
-    },
-    {
-      id: "484f4749-e366-474f-be17-fab60d07beb9",
-      title: "Opinion on the new product",
-      transcription:
-        "I am very happy with the new product. I would like to give a 5 star rating to the agent. Thank you.",
-      type: "video",
-      source: "live",
-      created_at: `${today.getHours()}:${today.getMinutes()}`,
-    },
-    {
-      id: "484f4749-e366-474f-be17-fab60d05beb0",
-      title: "By WebCam",
-      transcription: "capturing from WebCam.",
-      type: "video",
-      source: "webcam",
-      created_at: `${today.getHours()}:${today.getMinutes()}`,
-    },
-  ];
   return (
     <>
       {resultdb && (
